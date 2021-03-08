40050bfa7de6092ea524edc1e2e0a5863dd4ee47
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-03-08 13:26:55 +0100

    ViewComponent

---
+++ Movie/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie.Data;
+using Movie.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie.ViewComponents
+{
+   // [ViewComponent]
+    public class StarViewComponent : ViewComponent
+    {
+        private readonly MovieContext db;
+
+        public StarViewComponent(MovieContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int movieId)
+        {
+            var movie = await db.Movie.FindAsync(movieId);
+
+            var doubleRating = (int)Math.Round(movie.Rating * 2);
+
+            var model = new StarViewModel
+            {
+                Stars = doubleRating / 2,
+                IsHalfStar = doubleRating % 2 == 1
+            };
+
+            return View(model);
+        }
+    }
+}
